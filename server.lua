1c3030a867a2bbf87eabf1d7fe93cac60244fa72
joshllan26 <39708205+joshllan26@users.noreply.github.com>
2021-10-23 14:26:23 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,10 +1,31 @@
+local LevelRoles = {
+    -- {discord_role_id, perm_level}
+    {00000000001, 1},
+    {00000000002, 2},
+}
+
+-------
+
+local power = 0
 RegisterServerEvent('carspawning:GetPermissions')
 AddEventHandler('carspawning:GetPermissions', function(player)
     local plr = player
-    if plr < 3 then
-        local power = 20
-        TriggerClientEvent('carspawning:ReturnPermissions', source, power)
+    local playerRoles = exports.Badger_Discord_API:GetDiscordRoles(plr)
+
+    for i = 1, #playerRoles do
+        local prole = playerRoles[i]
+        for j = 1, #LevelRoles do
+            local role = LevelRoles[i]
+
+            if prole == role[1] then
+                if prole[2] > power then
+                    power = prole[2]
+                end
+            end
+        end
     end
+
+    TriggerClientEvent('carspawning:ReturnPermissions', source, power)
 end)
 
 RegisterCommand("spawn", function(source, args, rawCommand)
