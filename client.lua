71d7a3aaeb4a8f3d9f4ac44f385f0893e2a93120
joshllan26 <39708205+joshllan26@users.noreply.github.com>
2021-10-23 14:27:51 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,61 +1,53 @@
 local PlayerPowerLevel = 0
 
-    -- Callbacks --
-
+-- Callbacks --
 RegisterNUICallback('spawnveh', function(data, cb)
     Citizen.Trace(data.vehicle)
-    TriggerEvent("carspawning:carspawning", data.vehicle)
+    TriggerEvent('carspawning:carspawning', data.vehicle)
     SendNUIMessage({type = "close"})
 end)
-
-RegisterNUICallback('close', function(data, cb)
-    SetNuiFocus(false, false)
-end)
-
 RegisterNUICallback('refresh', function()
     Citizen.Trace("Refreshing carspawning UI")
     GetPlayerPower()
     Wait(500)
     SendNUIMessage({type = "refresh", power = PlayerPowerLevel})
 end)
-    -- Events --
+RegisterNUICallback('close', function(data, cb)
+    SetNuiFocus(false, false)
+end)
 
+-- Events --
 RegisterNetEvent('carspawning:carspawning')
 AddEventHandler('carspawning:carspawning', function(car)
     SpawnCar(car)
     Citizen.Trace("Spawning Car!")
 end)
-
 RegisterNetEvent('carspawning:ReturnPermissions')
 AddEventHandler('carspawning:ReturnPermissions', function(level)
     PlayerPowerLevel = level  
 end)
-
 RegisterNetEvent('carspawning:openGUI')
 AddEventHandler('carspawning:openGUI', function()
     OpenMenu()
     SendNUIMessage({type = "open"})
 end)
 
-    -- Functions --
-
+-- Functions --
 function GetPlayerPower()
     local player = source
     TriggerServerEvent('carspawning:GetPermissions', player)
-end
-
+end 
 function SpawnCar(v)
     local p = PlayerPedId()
-    
+        
     if IsPedInAnyVehicle(p, false) then
         local cv = GetVehiclePedIsIn(p, false)
         SetEntityAsNoLongerNeeded(cv)
         Citizen.Wait(10)
         DeleteEntity(cv)
-    end
-
+    end    
     local car = GetHashKey(v)
-
+    
     RequestModel(car)
     while not HasModelLoaded(car) do
         RequestModel(car)
@@ -65,10 +57,9 @@ function SpawnCar(v)
     local x,y,z = table.unpack(GetEntityCoords(p, false))
     local veh = CreateVehicle(car, x, y, z, p, true, false)
     SetEntityAsMissionEntity(veh, true, true)
-
+    
     SetPedIntoVehicle(p, veh, -1)
-end
-
+end    
 function OpenMenu()
     GetPlayerPower()
     Wait(500)
