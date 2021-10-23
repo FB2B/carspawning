5751da63f01fffb5ec16072522a8f381848189e4
joshllan26 <39708205+joshllan26@users.noreply.github.com>
2021-10-23 14:27:24 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,17 @@
-fx_version "bodacious"
-game "gta5"
+fx_version 'bodacious'
+game 'gta5'
 
-author "Mac"
-description "/spawn menu"
-version "1.0.0"
-
-client_script "client.lua"
-server_script "server.lua"
+author 'Mac'
+description 'A vehicle spawning menu for FiveM servers utilising html NUI.'
+version '1.0.0.1'
 
 ui_page 'ui/index.html'
 
 files {
-	'ui/index.html',
-	'ui/style.css'
+    'ui/index.html',
+    'ui/script.js',
+    'ui/style.css'
 }
+
+client_script 'client.lua'
+server_script 'server.lua'
